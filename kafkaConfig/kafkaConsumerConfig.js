553cb4293d2f38e195964aa8b45d390d900d2eb6
20058967 <fauzan.rachmatullah@btpn.com>
2024-02-24 20:05:03 +0700

    remove commented code

--- kafkaConfig/kafkaConsumerConfig.js
+++ kafkaConfig/kafkaConsumerConfig.js
@@ -7,16 +7,13 @@ const kafka = new Kafka({
   groupId: 'my-group',
 });
 
-// Buat instance consumer
 const consumer = kafka.consumer({
   groupId: 'my-group'
 });
 
-// Subscribe dan mulai mengonsumsi pesan
 async function startConsumer() {
   await consumer.connect();
   await consumer.subscribe({ topic: 'data_test' });
-  console.log('halohalo');
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
         console.log(`Received message from topic ${topic}, partition ${partition}, offset ${message.offset}: ${message.value.toString()}`);
