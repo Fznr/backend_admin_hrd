fbcbe68e14c420e174cc4cb925472580173dfe27
20058967 <fauzan.rachmatullah@btpn.com>
2024-02-24 19:46:44 +0700

    refactor code and add admin at the initial start

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import db from "../config/db.js";
+import {photoAdminBlob} from '../constant.js'
 
 const {DataTypes} = Sequelize;
 
@@ -13,28 +14,32 @@ const Employee = db.define('Employee', {
         type: DataTypes.STRING,
         allowNull: false
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      position: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      photo: {
-        type: DataTypes.BLOB
-      },
-      phoneNumber: {
-        type: DataTypes.STRING
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
       }
+    },
+    position: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    photo: {
+      type: DataTypes.BLOB
+    },
+    phoneNumber: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 }, {freezeTableName: true
 });
 
@@ -42,4 +47,24 @@ export default Employee;
 
 (async () => {
     await db.sync();
+
+    const adminExists = await Employee.findOne({ where: { role: 'Admin' } });
+    
+    if (!adminExists) {
+        try {
+            // Data admin
+            const adminData = {
+                name: 'Admin HRD',
+                email: 'adminHRD@argon.com',
+                position: "HR",
+                photo: photoAdminBlob,
+                password: 'adminPassword123',
+                role: 'Admin'
+            };
+            const admin = await Employee.create(adminData);
+            console.log('Admin data created:', admin.toJSON());
+        } catch (error) {
+            console.error('Error creating admin data:', error);
+        }
+    }
 })();
