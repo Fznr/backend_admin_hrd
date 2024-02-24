749570481a74cad253b6c64a33413ba883502a8d
20058967 <fauzan.rachmatullah@btpn.com>
2024-02-24 12:26:20 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import router from './routers/adminHrdRouter.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
