7ca06cee1d7ee5c585cf3baf103bfec96dd2751b
20058967 <fauzan.rachmatullah@btpn.com>
2024-02-28 08:37:53 +0700

    add dependencies and use body parser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import router from './routers/adminHrdRouter.js';
 import { startConsumer } from './kafkaConfig/kafkaConsumerConfig.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cors());
 app.use(router);
 
 startConsumer();
